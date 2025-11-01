6d457cdcec4176247ee6a2e53f8af80e32779804
nctri <congtri200512@gmail.com>
2025-11-01 15:05:26 +0700

    lan thu hai lam chuyena ay

---
+++ GT/SEARCH/linearSearch.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//thêm code lên serve 2h58
+void linearSearch(int a[], int n)
+{
+
+
+}
+
+int main() {
+
+
+
+
+
+    system("pause");
+    return 0;
+}
